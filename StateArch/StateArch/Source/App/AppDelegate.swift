d0bd274aed353b5c7cc1b238aef78d828e43e220
Jimmy Bouker <james.bouker@gmail.com>
2017-10-29 11:22:42 -0400

    BuddyBuild setup complete

--- StateArch/StateArch/Source/App/AppDelegate.swift
+++ StateArch/StateArch/Source/App/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        BuddyBuildSDK.setup()
         return true
     }
 }
